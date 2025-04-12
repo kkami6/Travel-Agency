de26aeb6f81f52b8d4376975c4ac29263647472e
DandelionChan <80709919+DandelionChan@users.noreply.github.com>
2025-04-12 16:19:51 +0300

    Added a few more tests that are successfully running.

--- TestingLayer/ExcursionsContextTests.cs
+++ TestingLayer/ExcursionsContextTests.cs
@@ -20,11 +20,70 @@ namespace TestingLayer
             excursionsContext = new ExcursionsContext(TestManager.dbContext);
         }
 
-        //[Test]
+        [Test]
 
-        //public void CreateExcursion()
-        //{
-        //    Excursion newExcursion = new Excursion(1, 1, 1730, 5,);
-        //}
+        public void CreateExcursion()
+        {
+            Excursion newExcursion = new Excursion(1, 1, 1730, DateTime.Now, DateTime.Now);
+            
+            excursionsContext.Create(newExcursion);
+
+            Excursion excursion = TestManager.dbContext.Excursions.Last();
+            Assert.That(newExcursion.ExcursionId, Is.EqualTo(excursion.ExcursionId), "The Ctreate() method does not work. :(");
+        }
+
+        [Test]
+        public void ReadExcursion()
+        {
+            Excursion newExcursion = new Excursion(1, 1, 1730, DateTime.Now, DateTime.Now);
+            excursionsContext.Create(newExcursion);
+
+            Excursion excursion = excursionsContext.Read(1);
+
+            Assert.That(newExcursion.ExcursionId == excursion.ExcursionId || newExcursion.Price == excursion.Price,
+                "The Read() method does not work! :(");
+        }
+
+        [Test]
+        public void ReadAllExcursions()
+        {
+            int excursionsBefore = TestManager.dbContext.Excursions.Count();
+
+            int excursionsAfter = excursionsContext.ReadAll().Count;
+
+            Assert.That(excursionsBefore == excursionsAfter, "The ReadAll() method does not work :(");
+        }
+
+        [Test]
+        public void UpdateExcursion()
+        {
+            Excursion newExcursion = new Excursion(1, 1, 1730, DateTime.Now, DateTime.Now);
+            excursionsContext.Create(newExcursion);
+
+            Excursion lastExcursion = excursionsContext.ReadAll().Last();
+            lastExcursion.Price = 2050;
+
+            excursionsContext.Update(lastExcursion);
+
+            Assert.That(excursionsContext.Read(lastExcursion.ExcursionId).Price == 2050, "The update() method does not work! :(");
+        }
+
+        [Test]
+
+        public void DeleteExcursion()
+        {
+            Excursion newExcursion = new Excursion(1, 1, 1730, DateTime.Now, DateTime.Now);
+            excursionsContext.Create(newExcursion);
+
+            List<Excursion> excursions = excursionsContext.ReadAll();
+            int excursionsBefore = excursions.Count();
+            Excursion lastExcursion = excursions.Last();
+
+            excursionsContext.Delete(lastExcursion.ExcursionId);
+
+            int excursionsAfter = excursionsContext.ReadAll().Count();
+
+            Assert.That(excursionsBefore == excursionsAfter + 1, "The Delete() method does not work! :(");
+        }
     }
 }
---
+++ TestingLayer/HotelsContextTests.cs
@@ -0,0 +1,85 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace TestingLayer
+{
+    [TestFixture]
+    internal class HotelsContextTests
+    {
+        HotelsContext hotelsContext;
+
+        public HotelsContextTests()
+        {
+            hotelsContext = new HotelsContext(TestManager.dbContext);
+        }
+
+        [Test]
+        public void CreateHotel()
+        {
+            Hotel newHotel = new Hotel(1, "str.Tsarevets 11", "Star Hotel", 4, false, true);
+
+            hotelsContext.Create(newHotel);
+
+            Hotel lastHotel = TestManager.dbContext.Hotels.Last();
+            Assert.That(newHotel.HotelId, Is.EqualTo(lastHotel.HotelId), "The Create() method does not work! :(");
+        }
+
+        [Test]
+        public void ReadHotel()
+        {
+            Hotel newHotel = new Hotel(1, "str.Tsarevets 11", "Star Hotel", 4, false, true);
+            hotelsContext.Create(newHotel);
+
+            Hotel hotel = hotelsContext.Read(1);
+
+            Assert.That(newHotel.Name == hotel.Name, "The Read() method does not work! :(");
+        }
+
+        [Test]
+        public void ReadAllHotels()
+        {
+            int hotelsBefore = TestManager.dbContext.Hotels.Count();
+
+            int hotelsAfter = hotelsContext.ReadAll().Count;
+
+            Assert.That(hotelsBefore == hotelsAfter, "The ReadAll() method does not work. :(");
+        }
+
+        [Test]
+
+        public void UpdateHotel()
+        {
+            Hotel newHotel = new Hotel(1, "str.Tsarevets 11", "Star Hotel", 4, false, true);
+            hotelsContext.Create(newHotel);
+
+            Hotel lastHotel = hotelsContext.ReadAll().Last();
+            lastHotel.Name = "Universal Hotel";
+
+            hotelsContext.Update(lastHotel);
+
+            Assert.That(lastHotel.Name == "Universal Hotel", "The update() method does not work. :(");
+        }
+
+        [Test]
+
+        public void DeleteHotel() 
+        {
+            Hotel newHotel = new Hotel(1, "str.Tsarevets 11", "Star Hotel", 4, false, true);
+            hotelsContext.Create(newHotel);
+
+            List<Hotel> hotels = hotelsContext.ReadAll();
+            int hotelsBefore = hotels.Count();
+            Hotel lastHotel = hotels.Last();
+
+            hotelsContext.Delete(lastHotel.HotelId);
+
+            int hotelsAfter = TestManager.dbContext.Hotels.Count();
+            Assert.That(hotelsBefore == hotelsAfter + 1, "The delete() method does not work. :(");
+        }
+    }
+}
