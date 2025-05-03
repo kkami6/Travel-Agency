7d15235124fe62c656e83a7bc81ee8bde28a46e4
e1its4 <elitsaa.sp@gmail.com>
2025-05-03 21:21:36 +0300

    Upgrading...

--- PresentationLayerUpg/CityForm.Designer.cs
+++ PresentationLayerUpg/CityForm.Designer.cs
@@ -28,58 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.nameLabel = new System.Windows.Forms.Label();
-            this.nameTxtBox = new System.Windows.Forms.TextBox();
-            this.countryLabel = new System.Windows.Forms.Label();
-            this.countryTxtBox = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            nameLabel = new Label();
+            nameTxtBox = new TextBox();
+            countryLabel = new Label();
+            countryTxtBox = new TextBox();
+            createBtn = new Button();
+            updateBtn = new Button();
+            deleteBtn = new Button();
+            dataGridView1 = new DataGridView();
+            testManagerBindingSource = new BindingSource(components);
+            cityBindingSource = new BindingSource(components);
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            countryDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)testManagerBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cityBindingSource).BeginInit();
+            SuspendLayout();
             // 
             // nameLabel
             // 
-            this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(41, 40);
-            this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(51, 20);
-            this.nameLabel.TabIndex = 0;
-            this.nameLabel.Text = "Name";
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new Point(46, 50);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new Size(59, 25);
+            nameLabel.TabIndex = 0;
+            nameLabel.Text = "Name";
             // 
             // nameTxtBox
             // 
-            this.nameTxtBox.Location = new System.Drawing.Point(98, 37);
-            this.nameTxtBox.Name = "nameTxtBox";
-            this.nameTxtBox.Size = new System.Drawing.Size(126, 26);
-            this.nameTxtBox.TabIndex = 1;
+            nameTxtBox.Location = new Point(109, 46);
+            nameTxtBox.Margin = new Padding(3, 4, 3, 4);
+            nameTxtBox.Name = "nameTxtBox";
+            nameTxtBox.Size = new Size(140, 31);
+            nameTxtBox.TabIndex = 1;
             // 
             // countryLabel
             // 
-            this.countryLabel.AutoSize = true;
-            this.countryLabel.Location = new System.Drawing.Point(28, 96);
-            this.countryLabel.Name = "countryLabel";
-            this.countryLabel.Size = new System.Drawing.Size(64, 20);
-            this.countryLabel.TabIndex = 2;
-            this.countryLabel.Text = "Country";
+            countryLabel.AutoSize = true;
+            countryLabel.Location = new Point(31, 120);
+            countryLabel.Name = "countryLabel";
+            countryLabel.Size = new Size(75, 25);
+            countryLabel.TabIndex = 2;
+            countryLabel.Text = "Country";
             // 
             // countryTxtBox
             // 
-            this.countryTxtBox.Location = new System.Drawing.Point(98, 96);
-            this.countryTxtBox.Name = "countryTxtBox";
-            this.countryTxtBox.Size = new System.Drawing.Size(126, 26);
-            this.countryTxtBox.TabIndex = 3;
+            countryTxtBox.Location = new Point(109, 120);
+            countryTxtBox.Margin = new Padding(3, 4, 3, 4);
+            countryTxtBox.Name = "countryTxtBox";
+            countryTxtBox.Size = new Size(140, 31);
+            countryTxtBox.TabIndex = 3;
+            // 
+            // createBtn
+            // 
+            createBtn.Location = new Point(137, 259);
+            createBtn.Name = "createBtn";
+            createBtn.Size = new Size(120, 54);
+            createBtn.TabIndex = 4;
+            createBtn.Text = "Create";
+            createBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateBtn
+            // 
+            updateBtn.Location = new Point(312, 259);
+            updateBtn.Name = "updateBtn";
+            updateBtn.Size = new Size(120, 54);
+            updateBtn.TabIndex = 5;
+            updateBtn.Text = "Update";
+            updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            deleteBtn.Location = new Point(481, 259);
+            deleteBtn.Name = "deleteBtn";
+            deleteBtn.Size = new Size(120, 54);
+            deleteBtn.TabIndex = 6;
+            deleteBtn.Text = "Delete";
+            deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nameDataGridViewTextBoxColumn, countryDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = cityBindingSource;
+            dataGridView1.Location = new Point(98, 329);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(364, 206);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // testManagerBindingSource
+            // 
+            testManagerBindingSource.DataSource = typeof(TestingLayer.TestManager);
+            // 
+            // cityBindingSource
+            // 
+            cityBindingSource.AllowNew = true;
+            cityBindingSource.DataSource = typeof(BusinessLayer.City);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // countryDataGridViewTextBoxColumn
+            // 
+            countryDataGridViewTextBoxColumn.DataPropertyName = "Country";
+            countryDataGridViewTextBoxColumn.HeaderText = "Country";
+            countryDataGridViewTextBoxColumn.MinimumWidth = 8;
+            countryDataGridViewTextBoxColumn.Name = "countryDataGridViewTextBoxColumn";
+            countryDataGridViewTextBoxColumn.Width = 150;
             // 
             // CityForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.countryTxtBox);
-            this.Controls.Add(this.countryLabel);
-            this.Controls.Add(this.nameTxtBox);
-            this.Controls.Add(this.nameLabel);
-            this.Name = "CityForm";
-            this.Text = "City";
-            this.Load += new System.EventHandler(this.CityForm_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(889, 562);
+            Controls.Add(dataGridView1);
+            Controls.Add(deleteBtn);
+            Controls.Add(updateBtn);
+            Controls.Add(createBtn);
+            Controls.Add(countryTxtBox);
+            Controls.Add(countryLabel);
+            Controls.Add(nameTxtBox);
+            Controls.Add(nameLabel);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "CityForm";
+            Text = "City";
+            Load += CityForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)testManagerBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cityBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
@@ -89,5 +176,13 @@
         private System.Windows.Forms.TextBox nameTxtBox;
         private System.Windows.Forms.Label countryLabel;
         private System.Windows.Forms.TextBox countryTxtBox;
+        private Button createBtn;
+        private Button updateBtn;
+        private Button deleteBtn;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn countryDataGridViewTextBoxColumn;
+        private BindingSource cityBindingSource;
+        private BindingSource testManagerBindingSource;
     }
 }
--- PresentationLayerUpg/ClientForm.cs
+++ PresentationLayerUpg/ClientForm.cs
@@ -8,18 +8,213 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ServiceLayer;
+using DataLayer;
+using Mysqlx;
 
 
 namespace WindowsFormsApp1
 {
     public partial class ClientForm : Form
     {
+        Client selectedClient;
+        bool updatedClient = false;
+        bool deleteClient = false;
+        int currentRowIndex = -1;
+        Manager<Client, int> ClientManager;
+
         public ClientForm()
         {
             InitializeComponent();
+            //selectedClient = new Client();
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validate
+                string name = nameTxtBox.Text;
+                string secondName = name2TxtBox.Text;
+                string familyName = name3TxtBox.Text;
+                string status = statusCheck.Checked ? "Loyal" : "Normal";
+                int age = DateTime.Now.Year - agePicker.Value.Year;
+
+                if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(secondName) || string.IsNullOrWhiteSpace(familyName))
+                {
+                    MessageBox.Show("Please fill in all fields!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                if (age < 18)
+                {
+                    MessageBox.Show("Client must be 18 or older!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                if (agePicker.Value.Date.Year == DateTime.Now.Year)
+                {
+                    MessageBox.Show("You must enter your birth date!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                Client client = new Client(name, secondName, familyName, age, status);
+
+                //serv layer
+                ClientManager.Create(client);
+                MessageBox.Show("Client added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                currentRowIndex = -1;
+
+                LoadClients();
+
+                nameTxtBox.Focus();
+
+                deleteBtn.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
 
         private void ClientForm_Load(object sender, EventArgs e)
+        {
+            //Load clients into the DataGridView
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            // Load clients logic
+            //clientDataGridView.DataSource = ClientManager.GetAllClients();
+            //this.clientTableAdapter.Fill(this.)
+        }
+
+        private void clientDataGridView_RowEnter(object sender, DataGridViewRowEventArgs e)
+        {
+            //MessageBox.Show("Text to show", "Caption", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (currentRowIndex == -1 || deleteClient)
+            {
+                currentRowIndex = 0;
+                return;
+            }
+
+            if (updatedClient)
+            {
+                ClientManager.Update(selectedClient);
+            }
+
+            //currentRowIndex = e.RowIndex;
+
+            selectedClient = new Client();
+
+            selectedClient.ClientId = ....Client[currentRowIndex].ClientId;
+            selectedClient.Name = .Client[currentRowIndex].Name;
+            selectedClient.SecondName = .Client[currentRowIndex].SecondName;
+            selectedClient.FamilyName = .Client[currentRowIndex].FamilyName;
+            selectedClient.Age = .Client[currentRowIndex].Age;
+            selectedClient.Status = .Client[currentRowIndex].Status;
+            //selectedClient.Excursions = .Client[currentRowIndex].Excursions;
+
+            //Update graphical components
+            nameTxtBox.Text = selectedClient.Name;
+            name2TxtBox.Text = selectedClient.SecondName;
+            name3TxtBox.Text = selectedClient.FamilyName;
+            statusCheck.Checked = selectedClient.Status;
+            agePicker.Value = selectedClient.Age;
+
+            updatedClient = false;
+            deleteBtn.Enabled = true;
+        }
+
+        private void clientDataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1 && e.ColumnIndex != -1)
+            {
+                clientDataGridView.UpdateCellValue(e.ColumnIndex, e.RowIndex);
+
+                switch (e.ColumnIndex)
+                {
+                    case 0:
+                        selectedClient.Name = .Client[e.RowIndex].Name;
+                        nameTxtBox.Text = selectedClient.Name;
+                        break;
+                    case 1:
+                        selectedClient.SecondName = .Client[e.RowIndex].SecondName;
+                        name2TxtBox.Text = selectedClient.SecondName;
+                        break;
+                    case 2:
+                        selectedClient.FamilyName = .Client[e.RowIndex].FamilyName;
+                        name3TxtBox.Text = selectedClient.FamilyName;
+                        break;
+                    case 3:
+                        selectedClient.Age = .Client[e.RowIndex].Age;
+                        agePicker.Value = selectedClient.Age;
+                        break;
+                    case 4:
+                        selectedClient.Status = .Client[e.RowIndex].Status;
+                        statusCheck.Checked = selectedClient.Status;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                updatedClient = true;
+            }
+        }
+
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            selectedClient.Name = nameTxtBox.Text;
+            selectedClient.SecondName = name2TxtBox.Text;
+            selectedClient.FamilyName = name3TxtBox.Text;
+            selectedClient.Age = agePicker.Value;
+            selectedClient.Status = statusCheck.Checked;
+
+            ClientManager.Update(selectedClient);
+
+            //Update
+            clientDataGridView[0, currentRowIndex].Value = selectedClient.Name;
+            clientDataGridView[1, currentRowIndex].Value = selectedClient.SecondName;
+            clientDataGridView[2, currentRowIndex].Value = selectedClient.FamilyName;
+            clientDataGridView[3, currentRowIndex].Value = selectedClient.Age;
+            clientDataGridView[4, currentRowIndex].Value = selectedClient.Status;
+
+            MessageBox.Show("Client updated successfuly!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            deleteClient = true;
+
+            clientDataGridView.Rows.RemoveAt(clientDataGridView.CurrentRow.Index);
+
+            ClientManager.Delete(selectedClient.ClientId);
+
+            deleteBtn.Enabled = false;
+            deleteClient = false;
+
+            MessageBox.Show("Client deleted successfuly!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ClearUserInterface();
+            //.Clear();
+
+            currentRowIndex = -1;
+
+            LoadClient();
+        }
+
+        private void ClearUserInterface()
+        {
+            nameTxtBox.Text = string.Empty;
+            name2TxtBox.Text =string.Empty;
+            name3TxtBox.Text = string.Empty;
+            agePicker.Value = DateTime.Now;
+            statusCheck.Checked = string.Empty;
+        }
+        private void clientDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
@@ -29,26 +224,6 @@ namespace WindowsFormsApp1
 
         }
 
-        private void createBtn_Click(object sender, EventArgs e)
-        {
-            try
-            {
 
-                //Validate
-                string name = nameTxtBox.Text;
-                string secondName = name2TxtBox.Text;
-                string familyName = name3TxtBox.Text;
-                string status = statusCheck.Checked ? "Loyal" : "Normal";
-                int age = 5;
-
-                Client client = new Client(name, secondName, familyName, age, status);
-
-
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
     }
 }
